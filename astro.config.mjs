ac74b3474a38901d875e944b3ca68fd27ffb2c62
a9x <hey@a9x.pro>
2026-04-22 19:41:46 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://perish.ing",
   adapter: cloudflare(),
+  env: {
+    schema: {
+      DISCORD_WEBHOOK_URL: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      TURNSTILE_SECRET_KEY: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+      PUBLIC_TURNSTILE_SITE_KEY: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    },
+  },
   integrations: [sitemap()],
   output: "server",
 });
